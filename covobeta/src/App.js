3953161a5e6ea82b7b7202494c6507cf0b18a1de
CoVoCre <theophane.mayaud@covo.io>
2018-01-09 14:37:37 +0100

    Working, added footer

--- covobeta/src/App.js
+++ covobeta/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Footer from "./components/Footer/Footer.js";
 import logo from "./logo.png";
 import "./App.css";
 
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Footer />
       </div>
     );
   }
---
+++ covobeta/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div align="center">
+          <a href="/">CoVo 0.1.0 </a>
+          <br />
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
