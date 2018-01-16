2de816b00e0202067019c71a06bed5d48f1116e8
Dev <dev@MaysBook.local>
2018-01-16 22:11:58 +0100

    Adding the toolbar dependency

--- covobeta/src/App.js
+++ covobeta/src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from "react";
-import HeaderBar from "./components/HeaderBar/HeaderBar.js";
+import {
+  Toolbar,
+  ToolbarRow,
+  ToolbarSection,
+  ToolbarTitle,
+  Content
+} from "react-mdc-web/lib";
 import Footer from "./components/Footer/Footer.js";
 import "./App.css";
+
+{
+  /*import HeaderBar from "./components/HeaderBar/HeaderBar.js";*/
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HeaderBar />
-        <main>
+        {/*        <HeaderBar />*/}
+        <Toolbar fixed>
+          <ToolbarRow>
+            <ToolbarSection align="start">Burger</ToolbarSection>
+
+            <ToolbarSection>
+              <ToolbarTitle>CoVo</ToolbarTitle>
+            </ToolbarSection>
+
+            <ToolbarSection align="end">
+              <div>Dropdown </div>
+              <div>Some things </div>
+            </ToolbarSection>
+          </ToolbarRow>
+        </Toolbar>
+
+        {/* Adjust top margin for fixed toolbar */}
+        <Content fixed>
           <p className="App-intro">
             Yes, this website exists but it is not yet ready !!! We are busy
             making it great, so it can help everyone out there who needs to go
             about freely. Well, but of course, who needs this, really ?
           </p>
-        </main>
+        </Content>
 
         <Footer />
       </div>
