dd1d8302e4c7447b80f9b086935dd2efc8a88c19
Dev <dev@tsf-484-wpa-7-228.epfl.ch>
2018-01-18 12:51:00 +0100

    Worked towards using material-ui@next but might reverse and use react-mdc-web

--- covobeta/src/App.js
+++ covobeta/src/App.js
@@ -1,51 +1,71 @@
 import React, { Component } from "react";
-import {
-  Toolbar,
-  ToolbarRow,
-  ToolbarSection,
-  ToolbarTitle,
-  Content
-} from "react-mdc-web/lib";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import { withStyles } from "material-ui/styles";
+import lightBlue from "material-ui/colors/lightBlue";
+import pink from "material-ui/colors/pink";
+import red from "material-ui/colors/red";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import Button from "material-ui/Button";
+import IconButton from "material-ui/IconButton";
+import MenuIcon from "material-ui-icons/Menu";
 import Footer from "./components/Footer/Footer.js";
 import "./App.css";
 
 {
   /*import HeaderBar from "./components/HeaderBar/HeaderBar.js";*/
 }
-
+const covoTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: lightBlue[300],
+      main: lightBlue[500],
+      dark: lightBlue[700]
+    },
+    secondary: {
+      light: pink.A200,
+      main: pink.A400,
+      dark: pink.A700
+    },
+    error: {
+      main: red[500]
+    }
+  }
+});
+const styles = {
+  root: {
+    flex: 1
+  }
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={covoTheme}>
         {/*        <HeaderBar />*/}
-        <Toolbar fixed>
-          <ToolbarRow>
-            <ToolbarSection align="start">Burger</ToolbarSection>
-
-            <ToolbarSection>
-              <ToolbarTitle>CoVo</ToolbarTitle>
-            </ToolbarSection>
-
-            <ToolbarSection align="end">
-              <div>Dropdown </div>
-              <div>Some things </div>
-            </ToolbarSection>
-          </ToolbarRow>
-        </Toolbar>
+        <AppBar position="static">
+          <Toolbar classes={styles.root}>
+            <IconButton color="contrast" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography type="title" color="inherit">
+              CoVo
+            </Typography>
+            <Button color="contrast">Login</Button>
+          </Toolbar>
+        </AppBar>
 
         {/* Adjust top margin for fixed toolbar */}
-        <Content fixed>
-          <p className="App-intro">
-            Yes, this website exists but it is not yet ready !!! We are busy
-            making it great, so it can help everyone out there who needs to go
-            about freely. Well, but of course, who needs this, really ?
-          </p>
-        </Content>
+        <p>
+          Yes, this website exists but it is not yet ready !!! We are busy
+          making it great, so it can help everyone out there who needs to go
+          about freely. Well, but of course, who needs this, really ?
+        </p>
 
         <Footer />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
