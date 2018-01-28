cf3d787adcc2a1635bbf1f489976cdaaa380769f
CoVoCre <creator@covo.io>
2018-01-28 23:46:02 +0100

    Working on it

--- covobeta/src/App.js
+++ covobeta/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import { Button } from "rmwc/Button";
+import { Elevation } from "rmwc/Elevation";
 
 import CoVoToolbarAndDrawer from "./components/CoVoToolbarAndDrawer/CoVoToolbarAndDrawer.js";
 import Footer from "./components/Footer/Footer.js";
@@ -13,15 +14,25 @@ class App extends Component {
       <Router>
         <div id="covo-web-app">
           <div id="covo-header">
-            {" "}
             <CoVoToolbarAndDrawer />
           </div>
           <div id="covo-page-content">
-            <p className="app-intro">
-              Yes, this website exists but it is not yet ready !!! We are busy
-              making it great, so it can help everyone out there who needs to go
-              about freely. Well, but of course, who needs this, really ?
-            </p>
+            <Elevation z="10">
+              <p className="app-intro">
+                Yes, this website exists but it is not yet ready !!! We are busy
+                making it great, so it can help everyone out there who needs to
+                go about freely. Well, but of course, who needs this, really ?
+              </p>
+            </Elevation>
+            <Button raised theme="mdc-theme--secondary-bg">
+              Default
+            </Button>
+            <Button raised theme="mdc-theme--secondary-bg">
+              Default
+            </Button>
+            <Button raised theme="mdc-theme--secondary-bg">
+              Default
+            </Button>
             <Button raised theme="mdc-theme--secondary-bg">
               Default
             </Button>
