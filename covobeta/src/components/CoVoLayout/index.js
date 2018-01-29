7b950427ceae4929a5cbc790461a4efa3f72594a
CoVoCre <theophane.mayaud@covo.io>
2018-01-29 09:36:13 +0100

    Extracted layout

--- covobeta/src/components/CoVoLayout/index.js
+++ covobeta/src/components/CoVoLayout/index.js
@@ -2,21 +2,22 @@ import React, { Component } from "react";
 
 import CoVoToolbarAndDrawer from "../CoVoToolbarAndDrawer/CoVoToolbarAndDrawer.js";
 import Footer from "../Footer/Footer.js";
+import "./CoVoLayout.css";
 
 export function CoVoToolbar(props) {
   return (
-    <header className="covo-toolbar">
+    <header id="covo-toolbar">
       <CoVoToolbarAndDrawer />
       {props.children}
     </header>
   );
 }
 export function CoVoPageContent(props) {
-  return <div className="covo-page-content">{props.children}</div>;
+  return <div id="covo-page-content">{props.children}</div>;
 }
 export function CoVoFooter(props) {
   return (
-    <div className="covo-footer">
+    <div id="covo-footer">
       <Footer />
     </div>
   );
