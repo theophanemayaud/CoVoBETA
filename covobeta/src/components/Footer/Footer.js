b8a33a1ce5de914ee4f4d7888d3d1f4ceb831cdb
CoVoCre <theophane.mayaud@covo.io>
2018-02-02 11:31:28 +0100

    Build time now included on page !

--- covobeta/src/components/Footer/Footer.js
+++ covobeta/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import data from "./../../timestamp.json";
 import "./Footer.css";
 
 class Footer extends Component {
@@ -8,6 +9,7 @@ class Footer extends Component {
         <div align="center">
           <a href="/">
             <div className="homepage-link">CoVo 0.1.0 </div>
+            <div id="footer-timestamp">build at {data.latest}</div>
           </a>
         </div>
       </div>
