cff3bdc08d0c8eff8503b7e042c1598331eab93f
CoVoCre <creator@covo.io>
2018-04-11 23:11:18 +0200

    Added some google maps autocomplete on homepage !!!

--- covobeta/src/components/HomePage/index.js
+++ covobeta/src/components/HomePage/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 //Installed dependencies imports
 import { Button } from "rmwc/Button";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 
 //CoVo components imports
 
@@ -12,7 +16,35 @@ import "./HomePage.css";
 
 //Beginning of implementation
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address1: "Paris, France",
+      address2: "Beijing, China"
+    };
+    this.onChange1 = address1 => this.setState({ address1 });
+    this.onChange2 = address2 => this.setState({ address2 });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
+
   render() {
+    const inputProps1 = {
+      value: this.state.address1,
+      onChange: this.onChange1
+    };
+    const inputProps2 = {
+      value: this.state.address2,
+      onChange: this.onChange2
+    };
+
     return (
       <div className="covo-home-page">
         <p className="app-intro">
@@ -23,6 +55,13 @@ class HomePage extends Component {
         <Button raised className="mdc-theme--secondary-bg		">
           Default
         </Button>
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <PlacesAutocomplete inputProps={inputProps1} />
+            <PlacesAutocomplete inputProps={inputProps2} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
