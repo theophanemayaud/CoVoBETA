8514193f46a89a5e31194b3375f80b24791aba85
CoVoCre <theophane.mayaud@covo.io>
2018-07-26 12:41:14 +0200

    fix(PlacesAutocomplete):  declared callback func before api load

--- covobeta/src/createCoVoStore.js
+++ covobeta/src/createCoVoStore.js
@@ -9,34 +9,43 @@ import firebaseInit from "./firebaseInit";
 
 const firebase = firebaseInit();
 
+const rfConfig = { oneListenerPerPath: true };
+
 const createCoVoStore = () => {
-	//const store = createStoreWithMiddleware(reducer, load(), composeWithDevTools());
-	const store = createStore(
-		rootReducer /*Loading from LocalStorage happens during
+  //const store = createStoreWithMiddleware(reducer, load(), composeWithDevTools());
+  const store = createStore(
+    rootReducer /*Loading from LocalStorage happens during
         creation of the Redux store.*/,
-		load({
-			states: ["uiState", "userInfoAndSettings", "newTripInfo", "utils"]
-		}),
-		composeWithDevTools(
-			// firebase instance as first argument
-			// other store enhancers if any
-			reduxFirestore(firebase),
-			applyMiddleware(
-				/* Saving to LocalStorage is achieved using Redux
+    load({
+      states: [
+        "uiState",
+        "userInfoAndSettings",
+        "newTripInfo",
+        "utils",
+        "firestore.ordered"
+      ]
+    }),
+    composeWithDevTools(
+      // firebase instance as first argument
+      // other store enhancers if any
+      reduxFirestore(firebase, rfConfig),
+      applyMiddleware(
+        /* Saving to LocalStorage is achieved using Redux
                 middleware.The 'save' method is called by Redux
                 each time an action is handled by your reducer. */
-				save({
-					states: [
-						"uiState",
-						"userInfoAndSettings",
-						"newTripInfo",
-						"utils"
-					]
-				})
-			)
-		)
-	);
-	return store;
+        save({
+          states: [
+            "uiState",
+            "userInfoAndSettings",
+            "newTripInfo",
+            "utils",
+            "firestore.ordered"
+          ]
+        })
+      )
+    )
+  );
+  return store;
 };
 
 export default createCoVoStore;
